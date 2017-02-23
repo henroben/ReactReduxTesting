1a8fbb71a61f49c7d244eee9618f5882fc88c52c
Ben Mercer <ben@henroben.co.uk>
2017-02-23 10:56:56 +0000

    S2:L15 - Form submit tests

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react';
 
 export default class CommentBox extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = { comment: '' };
+    }
+
+    onChangeHandler(event) {
+        this.setState({
+            comment: event.target.value
+        });
+    }
+
+    onSubmitHandler(event) {
+        event.preventDefault();
+
+        this.setState({
+            comment: ''
+        });
+    }
+
     render() {
         return(
-            <div className="comment-box">
-                <textarea name="commentBox" id="commentBox" cols="30" rows="10"></textarea>
-                <button>Submit Comment</button>
-            </div>
+            <form onSubmit={this.onSubmitHandler.bind(this)} className="comment-box">
+                <textarea name="commentBox" id="commentBox" cols="30" rows="10"
+                          onChange={this.onChangeHandler.bind(this)}
+                          value={this.state.comment} />
+                <button action="submit">Submit Comment</button>
+            </form>
         );
     }
 
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -35,4 +35,27 @@ describe('CommentBox', () => {
 
     });
 
+    describe('TextArea', () => {
+
+        beforeEach(() => {
+
+            component.find('textarea').simulate('change', 'new comment');
+
+        });
+
+        it('shows the text that is entered', () => {
+
+            expect(component.find('textarea')).to.have.value('new comment');
+
+        });
+
+        it('when submitted, clears the input', ()=> {
+
+            component.simulate('submit');
+            expect(component.find('textarea')).to.have.value('');
+
+        });
+
+    });
+
 });
