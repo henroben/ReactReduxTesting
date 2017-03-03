66637729447801bc329c5abc2762e7026c2d080e
Ben Mercer <ben@henroben.co.uk>
2017-03-03 14:26:37 +0000

    ssh test

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
 
     constructor(props) {
         super(props);
@@ -16,6 +18,8 @@ export default class CommentBox extends Component {
 
     onSubmitHandler(event) {
         event.preventDefault();
+
+        this.props.saveComment(this.state.comment);
 
         this.setState({
             comment: ''
@@ -34,3 +38,5 @@ export default class CommentBox extends Component {
     }
 
 }
+
+export default connect(null, actions)(CommentBox);
